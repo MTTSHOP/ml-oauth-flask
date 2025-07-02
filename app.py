c85bb0ef671adcc1c17cdea3f8f39edf68f71ecb
Max Pereira <max.cwb@gmail.com>
2025-07-02 13:41:49 -0300

    vvvv

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ def fetch_sale_prices(item_ids, access_token):
         }
         try:
             r = requests.get(url, params=params, timeout=5)
+            print(r)
         except requests.RequestException as exc:
             print(f"[SALE_PRICE] erro de rede para {iid}: {exc}")
             continue
